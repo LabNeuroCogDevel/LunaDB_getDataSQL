f7f6becc73be7384c4c4152daae9dc71ffdc0c3a
Will Foran <willforan@gmail.com>
2015-03-30 09:44:50 -0400

    use more sql/less bash for cantab lookup/table merge

--- OHJF-demo/cantab_IQ_quest.bash
+++ OHJF-demo/cantab_IQ_quest.bash
@@ -1,26 +1,33 @@
 #!/usr/bin/env bash
 dbi="mysql://localadmin:local!123@arnold.wpic.upmc.edu/lncddb3"
-# sql "$dbi" "create temporary table mytable (luna varchar(10), visitdate date); load data local infile 'CANTAB_LunaIDsForDemos.csv' into table mytable Fields TERMINATED BY ','; select * from mytable"
+
 perl -F, -slane 'next if $.==1;
   print "$F[0] ", join( "-", map {sprintf "%02s", $_} (split/\//,$F[1])[2,0,1])
- ' CANTAB_LunaIDsForDemos.csv | 
- while read luna vd; do
+ ' CANTAB_LunaIDsForDemos.csv > luna_date.txt 
 
-   query="
-     select p.value as lunaid, abs(datediff(visitdate,'$vd')/365.25) as visitdiffyears, vt.taskName,vt.subsection, vt.value
-      from peopleEnroll p
+sql "$dbi" \
+"
+create temporary table cantabvisit (luna varchar(10), visitdate date);
+load data local infile 'luna_date.txt' into table cantabvisit Fields TERMINATED BY ' ';
+select p.value as lunaid, abs(datediff(v.visitdate,cv.visitdate)/365.25) as visitdiffyears, vt.taskName,vt.subsection, vt.value
+      from cantabvisit as cv
+      join peopleEnroll p
+       on p.value=cv.luna
       join visits v 
        on  p.peopleID=v.peopleID
       join visitstasks vt 
        on v.visitID=vt.visitID
-      where enrollType like 'LunaID' and
-            p.value like '$luna' and
-            subsection in ('DMSDifficulty','DMSStrategy',
+      where 
+        enrollType like 'LunaID' and
+        subsection in ('DMSDifficulty','DMSStrategy',
                   'SSPDifficulty' ,'SSPStrategy',
                   'SOCDifficulty','SOCStrategy',
                   'wfull2','wfull4','wvoc','wperf')
-"
-   sql $dbi "$query" 
-done | tee allCANTAB.txt 
+" | tee allQuest_WASI_forCANTAB.txt 
 
-Rio -d"\t" ' df %>% group_by(lunaid,taskName,subsection) %>% filter(rank(visitdiffyears)==1) %>% spread' < allCANTAB.txt  > sorted
+Rio -d"\t" 'df %>%
+ group_by(lunaid,taskName,subsection) %>% 
+ filter(rank(visitdiffyears,ties.method="first")==1) %>%
+ unite(ts,taskName,subsection) %>%
+ select(-visitdiffyears) %>%
+ spread(ts,value)' < allQuest_WASI_forCANTAB.txt > closestsToCANTAB.txt
